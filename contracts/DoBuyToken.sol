bccc1273db7ce5a8bcf0a986aadba450e72ed787
kyjin8 <kyjin9705@gmail.com>
2021-08-19 15:32:56 +0900

    배송 파트 작업중

--- contracts/DoBuyToken.sol
+++ contracts/DoBuyToken.sol
@@ -2,8 +2,10 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DoBuyToken is ERC20 {
+    using Counters for Counters.Counter;
 
     /// @notice 토큰 이름
     string public constant _token = "DoBuyToken";
@@ -20,8 +22,25 @@ contract DoBuyToken is ERC20 {
     /// @notice 유저 지갑
     address public user_wallet = payable(msg.sender);
 
+    /// @notice 운송장번호
+    Counters.Counter public _trackingIds;
+
+    /// @dev 운송장 정보
+    struct deliveryBill {
+        uint256 _Ids;   //trackingId
+        uint16 amount;
+        string sender;
+        string receiver;
+        string name;
+    }
+
     constructor() ERC20("DoBuyToken", "DBT") {
         address owners = msg.sender;
         _mint(owners, INITIAL_SUPPLY * 10 ** (uint(decimals())));
+    }
+    
+    /// @dev delivery - 배송 시작
+    function deliveryStart(uint256 NFT) public {
+        _trackingIds.increment();
     }
 }
